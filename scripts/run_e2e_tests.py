b2ff053bf4157c694030ac2c8274341c3a158266
Bart Broere <mail@bartbroere.eu>
2023-11-25 21:04:22 +0100

    Update run_e2e_tests.py

--- scripts/run_e2e_tests.py
+++ scripts/run_e2e_tests.py
@@ -470,7 +470,7 @@ def run_e2e_tests(
                         w.write(f.read())
                     # add 2 MB of trailing zero bytes to exceed the limit for files 
                     # in the static directory
-                    w.write(b'\x00' * 2 * 1024 * 1024)
+                    w.write(b'\xaa' * 2 * 1024 * 1024)
                 test_name, _ = splitext(basename(spec_path))
                 test_name, _ = splitext(test_name)
                 test_path = join(
